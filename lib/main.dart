fa5ac16554b31c64ed5cf1b9d6c103cdb6e43af3
danju4rizzl <danjumashiwaju@gmail.com>
2020-04-29 13:31:14 +0200

    added functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,49 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  // Get the sound of each note asset
+  getSound(soundNote) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+    assetsAudioPlayer.open(
+      Audio("assets/note$soundNote.wav"),
+    );
+  }
+
+// Renders (returns) the keys in an expanded widget
+  renderKey({dynamic keyColor, int sound}) {
+    return Expanded(
+      child: FlatButton(
+        color: keyColor,
+        onPressed: () {
+          getSound(sound);
+        }, // child: Text('$note'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(
-            child: Text(nouns.first),
-          ),
-        ),
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            renderKey(keyColor: Colors.red, sound: 1),
+            renderKey(keyColor: Colors.orange, sound: 2),
+            renderKey(keyColor: Colors.yellow, sound: 3),
+            renderKey(keyColor: Colors.green, sound: 4),
+            renderKey(keyColor: Colors.teal, sound: 5),
+            renderKey(keyColor: Colors.blue, sound: 6),
+            renderKey(keyColor: Colors.purple, sound: 7),
+          ],
+        )),
       ),
     );
   }
